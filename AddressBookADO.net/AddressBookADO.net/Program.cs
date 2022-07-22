ab3d8ef1c453aba8fb76704244ab709b2ccab24c
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-22 15:11:46 +0530

    [Shivaraj] Added Creating Address Book Database

--- AddressBookADO.net/AddressBookADO.net/Program.cs
+++ AddressBookADO.net/AddressBookADO.net/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Welcome to AddressBook ADO.NET!");
+﻿namespace AddressBookADO.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.NET!");
+            Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase");
+            int op = Convert.ToInt16(Console.ReadLine());
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+
+            switch (op)
+            {
+                case 1:
+                    addressBookRepo.Create_Database();
+                    break;
+
+            }
+        }
+    }
+}
+
+/*
+UC-1
+Create AddrssBookServiceDatabase
+Welcome to AddressBook ADO.NET!
+Select option
+1)Create AddrssBookServiceDatabase
+1
+AddressbookService Database created successfully!
+*/
